68427d2d885fe9807ad7ed83daae3387b17c3cc4
nailuj05 <julian.limburg@gmx.de>
2021-02-09 17:59:35 +0100

    Worked on Resultspage

---
+++ results.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Results</title>
+    <link rel="stylesheet" href="include/css/results.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1 class="headline">Very nice Rechner</h1>
+    </div>
+
+    <div class="body">
+        <h2 class="results">Deine Ergebnisse</h2>
+    </div>
+</body>
+</html>
+
+<?php
+    if(isset($_GET['price'])){
+        $gender = $_GET['price'];
+        echo $gender; 
+    }
+?>
